f8c940f3328f8c8918ef5c3395ef678f30667549
JadejaVishwrajsinh7421 <JadejaVishwrajsinh7421>
2025-06-20 20:30:08 +0530

    "QUERY 4  WORK IS DONE"

---
+++ DU lab/SQLQuery4.sql
@@ -0,0 +1,154 @@
+--Alter, Rename Operation 
+
+USE CSE_3A_127;
+--Part – A: 
+--Use Deposit table of lab-1. 
+ 
+--DEPOSIT 
+--Column_Name DataType 
+--ACTNO INT 
+--CNAME VARCHAR(50) 
+--BNAME VARCHAR(50) 
+--AMOUNT DECIMAL(8,2) 
+--ADATE DATETIME 
+ 
+--From the above given tables perform the following queries (ALTER, RENAME Operation): 
+--1. Add two more columns City VARCHAR (20) and Pincode INT. 
+	ALTER TABLE DEPOSITE
+	ADD  CITY VARCHAR(20), PINCODE INT ;
+
+	SELECT *
+	FROM DEPOSITE
+
+--2. Add column state VARCHAR(20). 
+	ALTER TABLE DEPOSITE
+	ADD STATE VARCHAR(20)
+
+	SELECT *
+	FROM DEPOSITE
+
+--3. Change the size of CNAME column from VARCHAR (50) to VARCHAR (35).
+	ALTER TABLE DEPOSITE
+	ALTER  COLUMN CNAME VARCHAR (35)
+
+	SELECT *
+	FROM DEPOSITE
+
+--4. Change the data type DECIMAL to INT in amount Column. 
+	ALTER TABLE DEPOSITE
+	ALTER  COLUMN AMOUNT INT 
+
+	SELECT *
+	FROM DEPOSITE
+
+--5. Delete Column City from the DEPOSIT table. 
+	ALTER TABLE DEPOSITE
+	DROP COLUMN CITY
+
+	SELECT *
+	FROM DEPOSITE
+
+--6. Rename Column ActNo to ANO. 
+	SP_RENAME 'DEPOSITE.ACTNO','ANO'
+
+	SELECT *
+	FROM DEPOSITE
+--7. Change name of table DEPOSIT to DEPOSITDETAIL
+	SP_RENAME 'DEPOSITE','DEPOSITDETAIL'
+
+	SELECT *
+	FROM DEPOSITDETAIL
+. 
+--Part – B: 
+--1. Rename Column ADATE to AOPENDATE OF DEPOSITDETAIL table. 
+	SP_RENAME 'DEPOSITDETAIL.ADATE','AOPENDATE'
+
+	SELECT *
+	FROM DEPOSITDETAIL
+
+--2. Delete Column AOPENDATE from the DEPOSITDETAIL table. 
+	ALTER TABLE  DEPOSITDETAIL
+	DROP COLUMN   AOPENDATE 
+
+	SELECT *
+	FROM DEPOSITDETAIL
+
+--3. Rename Column CNAME to CustomerName.
+	SP_RENAME 'DEPOSITDETAIL.CNAME' , 'CustomerName.'
+
+	SELECT *
+	FROM DEPOSITDETAIL
+
+--4. Add Column country.
+	ALTER TABLE DEPOSITDETAIL
+	ADD COUNTRY VARCHAR(30)
+
+	SELECT *
+	FROM DEPOSITDETAIL
+
+--Part – C: 
+
+--Create following table using query according to the definition. 
+--STUDENTDETAIL 
+--Column_Name DataType 
+--Enrollment_No VARCHAR(20) 
+--Name VARCHAR(25) 
+--CPI DECIMAL(5,2) 
+--Birthdate DATETIME 
+
+CREATE TABLE STUDENTDETAIL (
+Enrollment_No VARCHAR(20),
+Name VARCHAR(25) ,
+CPI DECIMAL(5,2) ,
+Birthdate DATETIME);
+
+SELECT *
+FROM STUDENTDETAIL
+
+--From the above given tables perform the following queries (ALTER, RENAME Operation): 
+--1. Add two more columns City VARCHAR (20) (Not null) and Backlog INT (Null). 
+	ALTER TABLE STUDENTDETAIL
+	ADD City VARCHAR (20) NOT NULL,Backlog INT NULL
+
+	SELECT *
+	FROM STUDENTDETAIL
+
+--2. Add column department VARCHAR (20) Not Null.
+	ALTER TABLE STUDENTDETAIL
+	ADD DEPARTMENT  VARCHAR(20)
+
+	SELECT *
+	FROM STUDENTDETAIL
+
+--3. Change the size of NAME column of studentDETAIL from VARCHAR (25) to VARCHAR (35). 
+	ALTER TABLE STUDENTDETAIL
+	ALTER  COLUMN NAME VARCHAR(35)
+
+	SELECT *
+	FROM STUDENTDETAIL
+
+--4. Change the data type DECIMAL to INT in CPI Column. 
+	ALTER TABLE STUDENTDETAIL
+	ALTER  COLUMN CPI INT
+
+	SELECT *
+	FROM STUDENTDETAIL
+
+--5. Delete Column City from the studentDETAIL table.
+	ALTER TABLE STUDENTDETAIL
+	DROP COLUMN City
+
+	SELECT *
+	FROM STUDENTDETAIL
+--6. Rename Column Enrollment_No to ENO. 
+	SP_RENAME 'STUDENTDETAIL.Enrollment_No ' ,'ENO'
+
+	SELECT *
+	FROM STUDENTDETAIL
+
+--7. Change name of table studentDETAIL to STUDENT_MASTER.
+	SP_RENAME 'STUDENTDETAIL','STUDENTMASTER'
+		
+	SELECT *
+	FROM STUDENTMASTER
+
